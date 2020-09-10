2568c8d9aa9b01cf5b0a4c4a424d6dfa9855a476
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-09-10 21:35:27 +0300

    Добавление класса PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+public class PostManager {
+
+    private PostManager[] posts;
+
+    public PostManager[] search(int ownedId, String domain, String query, boolean ownersOnly, int count, int offset) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId){
+
+    }
+}
